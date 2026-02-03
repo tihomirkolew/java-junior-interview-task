596535dfa12b3d6fee29b95e449dbf2e254b1e06
Tihomir Kolev <tihomirkolew@gmail.com>
2026-02-03 22:54:55 +0200

    Initial commit

---
+++ src/main/java/java_junior_interview_task/JavaJuniorInterviewTaskApplication.java
@@ -0,0 +1,13 @@
+package java_junior_interview_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJuniorInterviewTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJuniorInterviewTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java_junior_interview_task/JavaJuniorInterviewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package java_junior_interview_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJuniorInterviewTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
