54285e977f8d536820162828244057e3e87ff23a
Tihomir Kolev <tihomirkolew@gmail.com>
2026-02-15 19:40:14 +0200

    Add protected and guest route components; update routing in App and SideBar for authentication-based access

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import EditUser from './components/editUser/EditUser'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
 import Logout from './components/logout/Logout'
+import ProtectedRoute from './components/protectedRoutes/ProtectedRoute'
+import GuestRoute from './components/protectedRoutes/GuestRoute'
 
 function App() {
 
@@ -20,14 +22,21 @@ function App() {
       <div className='content'>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/create" element={<CreateUser />} />
-          <Route path="/details" element={<UserDetails />} />
-          <Route path="/list" element={<UserList />} />
-          <Route path="/search" element={<UserSearch />} />
-          <Route path="/edit" element={<EditUser />} />
+
+          <Route element={<GuestRoute />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/create" element={<CreateUser />} />
+            <Route path="/details" element={<UserDetails />} />
+            <Route path="/list" element={<UserList />} />
+            <Route path="/search" element={<UserSearch />} />
+            <Route path="/edit" element={<EditUser />} />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+
         </Routes>
       </div>
     </>
--- client/src/components/sideBar/SideBar.jsx
+++ client/src/components/sideBar/SideBar.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router';
 import styles from './SideBar.module.css';
+import { useUserContext } from '../../contexts/UserContext';
 
 export default function SideBar() {
+    const { isAuthenticated } = useUserContext();
+
     return (
         <div className={styles.sidebar}>
             <div>
@@ -9,21 +12,31 @@ export default function SideBar() {
 
                 <nav className={styles.nav}>
                     <ul>
-                        <li><Link to="/create">Create user</Link></li>
-                        <li><Link to="/details">User details</Link></li>
-                        <li><Link to="/list">All users</Link></li>
-                        <li><Link to="/search">Search users</Link></li>
-                        <li><Link to="/edit">Edit user</Link></li>
-                        <li><Link to="/delete">Delete user</Link></li>
+                        {isAuthenticated && (
+                            <>
+                                <li><Link to="/create">Create user</Link></li>
+                                <li><Link to="/details">User details</Link></li>
+                                <li><Link to="/list">All users</Link></li>
+                                <li><Link to="/search">Search users</Link></li>
+                                <li><Link to="/edit">Edit user</Link></li>
+                            </>
+                        )}
+
                     </ul>
                 </nav>
             </div>
             <nav className={styles.nav}>
-            <hr className={styles.divider} />
+                <hr className={styles.divider} />
                 <ul>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/logout">Logout</Link></li>
+                    {!isAuthenticated && (
+                        <>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                        </>)
+                    }
+                    {isAuthenticated && (
+                        <li><Link to="/logout">Logout</Link></li>
+                    )}
                 </ul>
             </nav>
         </div>
