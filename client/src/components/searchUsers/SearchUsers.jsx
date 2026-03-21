34bdfc0e663a1659eb6062593a0700af77523dfe
Tihomir Kolev <tihomirkolew@gmail.com>
2026-03-21 14:11:26 +0200

    Implement user search functionality: add SearchUsers component with form handling, user fetching, and pagination; create SearchUsers.module.css for styling.

--- client/src/components/searchUsers/SearchUsers.jsx
+++ client/src/components/searchUsers/SearchUsers.jsx
@@ -1,8 +1,141 @@
+import { useNavigate } from 'react-router';
+import { useUserContext } from '../../contexts/UserContext';
+import styles from './SearchUsers.module.css'
+import { useState } from 'react';
+import UserCard from '../userCard/UserCard';
+
 export default function UserSearch() {
+    // todo: search user form
+
+    const { user, isAuthenticated } = useUserContext();
+    const navigate = useNavigate();
+
+    const [userData, setUserData] = useState([]);
+    const [values, setValues] = useState({});
+    const [errors, setErrors] = useState({});
+    const [totalPages, setTotalPages] = useState();
+    const [page, setPage] = useState(0);
+
+    if (!isAuthenticated) {
+        alert('You must be logged in to create a user!');
+        navigate('/login');
+        return;
+    }
+
+    const handleChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+
+        if (errors[e.target.name]) {
+            setErrors(state => ({
+                ...state,
+                [e.target.name]: null
+            }));
+        }
+    }
+
+    const fetchUsers = async (currentPage = 0) => {
+        const size = values.size || 0;
+
+        try {
+            const res = await fetch(
+                `http://localhost:8086/api/users/search?term=${values.param}&page=${currentPage}&size=${size}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa(user?.email + ':' + user?.plainPassword),
+                    }
+                }
+            );
+
+            if (!res.ok) {
+                const errorData = await res.json();
+                setErrors(errorData);
+                console.log(errorData);
+
+                return;
+            }
+
+            const data = await res.json();
+            console.log(data);
+
+            setUserData(data.content);
+            setTotalPages(data.totalPages);
+            setPage(currentPage);
+        } catch (error) {
+            alert(error.message);
+        }
+
+
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!values.param) {
+            setErrors(state => ({
+                ...state,
+                param: 'Param is required!'
+            }));
+            return;
+        }
+        await fetchUsers(0);
+    }
+
+    const handlePageChange = (newPage) => {
+        fetchUsers(newPage);
+    }
+
     return (
         <>
-            // todo: search user form
-            <h1>Search user</h1>
+            <div className={styles.searchUserContainer}>
+                <h1>Search user</h1>
+                <form
+                    id="search-user-form"
+                    className={styles.form}
+                    onSubmit={onSubmit}
+                >
+                    <div className={styles.inputContainer}>
+                        <div className={styles.searchInputField}>
+                            <input
+                                type="text"
+                                name="param"
+                                placeholder="Search"
+                                value={values.param || ''}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="number"
+                                name="size"
+                                placeholder="Results per page"
+                                value={values.size || ''}
+                                onChange={handleChange}
+                                min={1}
+                            />
+                            <button type="submit">Search</button>
+                        </div>
+                    </div>
+                    {errors.param && <p className={styles.textDanger}>{errors.param}</p>}
+                </form>
+
+                <div className={styles.userInfo}>
+                    {userData.map(u => <UserCard key={u.id} {...u} userListStyles={styles.userInfoContainer} />)}
+                </div>
+
+                {totalPages > 1 && (
+                    <div className={styles.pagination}>
+                        <div className={styles.paginationButtons}>
+                            <button onClick={() => handlePageChange(page - 1)} disabled={page === 0}>Prev</button>
+                            <button onClick={() => handlePageChange(page + 1)} disabled={page === totalPages - 1}>Next</button>
+                        </div>
+                        <span>{page + 1} / {totalPages}</span>
+                    </div>
+                )}
+            </div>
         </>
     );
 }
